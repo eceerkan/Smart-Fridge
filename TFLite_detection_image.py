b6eca4492bc451fc3252d58f7e3aea1c73038303
Ece Erkan <105375774+eceerkan@users.noreply.github.com>
2022-12-09 23:29:01 +0000

    Update TFLite_detection_image.py

--- TFLite_detection_image.py
+++ TFLite_detection_image.py
@@ -173,9 +173,9 @@ else: # This is a TF1 model
 while True:
 # Loop over every image and perform detection
     if  GPIO.event_detected(button_pin):
-            camera.capture('/home/pi/tflite_project/images/images%s.jpg' %(i))
-            img=cv2.imread('/home/pi/tflite_project/images/images%s.jpg' %(i))
-            image.append(img)
+          #  camera.capture('/home/pi/tflite_project/images/images%s.jpg' %(i))
+           # img=cv2.imread('/home/pi/tflite_project/images/images%s.jpg' %(i))
+            #image.append(img)
          #  img=cv2.rotate(image[i],cv2.ROTATE_180)
           #  cv2.imwrite('/home/pi/tflite_project/images/images%s.jpg' %(i), image[i])
             print(i)
