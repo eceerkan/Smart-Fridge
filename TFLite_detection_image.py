482f98a3e180cd9fafd8c1514063797d09252830
eceerkan <eceerkan71@gmail.com>
2022-12-10 00:58:05 +0000

    model tested with images

--- TFLite_detection_image.py
+++ TFLite_detection_image.py
@@ -1,4 +1,4 @@
-≈######## Webcam Object Detection Using Tensorflow-trained Classifier #########
+######## Webcam Object Detection Using Tensorflow-trained Classifier #########
 #
 # Author: Evan Juras
 # Date: 11/11/22
@@ -174,7 +174,7 @@ while True:
 # Loop over every image and perform detection
     if  GPIO.event_detected(button_pin):
           #  camera.capture('/home/pi/tflite_project/images/images%s.jpg' %(i))
-            img=cv2.imread('/home/pi/tflite_project/images/images%s.jpg' %(i)'+'/home/pi/tflite_project/images/images%s.jpeg' %(i)')
+            img=cv2.imread('/home/pi/Project/Smart-Fridge/images/images%s.jpeg' %(i))
             image.append(img)
          #  img=cv2.rotate(image[i],cv2.ROTATE_180)
           # cv2.imwrite('/home/pi/tflite_project/images/image%s.jpg' %(i), image[i])
