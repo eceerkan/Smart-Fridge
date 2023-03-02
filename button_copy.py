59d4a83d8300d2879931b5e45c8cf25e6a499b81
Ece Erkan <105375774+eceerkan@users.noreply.github.com>
2023-03-02 18:02:39 +0000

    Update button_copy.py

--- button_copy.py
+++ button_copy.py
@@ -12,17 +12,13 @@ if __name__=='__main__' :
         GPIO.add_event_detect(button_pin,GPIO.FALLING,bouncetime=100)
 
 # Connect to GitHub
-g = Github("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")
-repo = g.get_repo("eceerkan/Smart-Fridge")
-print(repo)
-# Get the contents of the text file in the repository
-contents = repo.get_contents("transfer.txt")
-
-count = int(contents.decoded_content)
 
 while True:
-	if  GPIO.event_detected(button_pin):
-        	count += 1
-               	repo.update_file(contents.path, "Updated count", str(count), contents.sha)
-		contents = repo.get_contents("transfer.txt")
-	  	print(contents)
+  if  GPIO.event_detected(button_pin):
+    g = Github("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")
+    repo = g.get_repo("eceerkan/Smart-Fridge")
+    contents = repo.get_contents("transfer.txt")
+    count = int(contents.decoded_content)
+    count += 1
+    repo.update_file(contents.path, "Updated count", str(count), contents.sha)
+    print(contents)
